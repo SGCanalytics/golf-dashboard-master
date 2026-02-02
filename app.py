235b210ec366602468cfee5e3612618347288a56
SGCanalytics <47985007+SGCanalytics@users.noreply.github.com>
2026-02-02 00:43:34 -0500

    Update app.py
    hole summary

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+from hole_summary import build_hole_summary
 
 # ============================================================
 # CONFIG
